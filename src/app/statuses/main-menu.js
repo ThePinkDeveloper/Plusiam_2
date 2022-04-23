aa8a0714c4d6b52b26f7c6916d02ad5e75e6d294
Miguel Rosa <nmlbstudio@gmail.com>
2022-04-23 13:57:55 +0200

    Added music and sound icon in main menu to enable music

--- src/app/statuses/main-menu.js
+++ src/app/statuses/main-menu.js
@@ -8,10 +8,13 @@ export class MainMenu {
 
     // Initialize game
     constructor(canvas) {
+        this.increment = 0;
         this.canvas = canvas;
         this.ctx = canvas.getContext('2d');
         this.mainMenuPanel = this.createMainMenuPanel();
-        this.title = this.createTitle();
+        this.titleImage = document.getElementById('titleImage');
+        this.soundEnabled = false;
+        this.soundIcon = document.getElementById('soundIcons');
         this.newGame = this.createTagNewGame();
         //this.tutorial = this.createTagTutorial();
         this.exit = this.createTagExit();
@@ -21,6 +24,7 @@ export class MainMenu {
         // Get the canvas context from parameter
         this.tags = []
         this.fillTagArray();
+        this.sound = this.loadMusic();
         
     }
 
@@ -28,7 +32,7 @@ export class MainMenu {
         this.clickedX = this.canvas.clickedX;
         this.clickedY = this.canvas.clickedY;
         if (this.clickedX != -1) {
-            // It controls if you clicked inside a block and get it
+            // It controls if you clicked inside a tag and get it
             const tagClicked = this.tags.find ( tag => 
                     this.clickedX > tag.x - tag.width / 2 
                 && this.clickedX < tag.x + tag.width / 2
@@ -38,15 +42,32 @@ export class MainMenu {
             if (!!tagClicked) {
                 return this.GAMEON;
             }
+            
+            // It controls if you clicked inside the sound icon
+            if (this.clickedX > 20
+                    && this.clickedX < 20 + 64
+                    && this.clickedY > 700 
+                    && this.clickedY < 700 + 64) {
+                this.soundEnabled = !this.soundEnabled;
+            }
+            
         }
+
+        if (this.soundEnabled) {
+            this.sound.play();
+        } else {
+            this.sound.pause();
+        }
+
         return this.MAINMENU;
     }
 
     draw() {
         this.mainMenuPanel.draw();
-        this.title.draw();
+        this.drawTitle();
         this.newGame.draw();
         //this.tutorial.draw();
+        this.drawSoundIcon();
         this.exit.draw();
     }
 
@@ -55,12 +76,20 @@ export class MainMenu {
         return new GeneralPanel(this);
     }
 
-    createTitle() {
-        return new Tag(this, 'PLUSIAM', 206, 100, 70, 'white', 'center');
+    drawTitle() {
+        this.ctx.drawImage(this.titleImage, 0, 0, this.titleImage.width, this.titleImage.height, 60, 120 + 30 * Math.sin(this.increment += .05), 300, 80);
+    }
+
+    drawSoundIcon() {
+        if (this.soundEnabled) {
+            this.ctx.drawImage(this.soundIcon, this.soundIcon.width / 2, 0, this.soundIcon.width / 2, this.soundIcon.height, 20, 700, 64, 64);
+        } else {
+            this.ctx.drawImage(this.soundIcon, 0, 0, this.soundIcon.width / 2, this.soundIcon.height, 20, 700, 64, 64);
+        }
     }
 
     createTagNewGame() {
-        return new Tag(this, 'New Game', 206, 320, 50, 'white', 'center');
+        return new Tag(this, 'New Game', 206, 370, 50, 'white', 'center');
     }
 
     createTagTutorial() {
@@ -73,5 +102,15 @@ export class MainMenu {
 
     fillTagArray() {
         this.tags.push(this.newGame, this.exit);
+    }
+
+    loadMusic() {
+        const sound = document.createElement("audio");
+        sound.src = '../../../assets/ambient-song-intro.menu-plusiam.ogg';
+        sound.setAttribute("preload", "auto");
+        sound.setAttribute("controls", "none");
+        sound.style.display = "none"; // <-- oculto
+        document.body.appendChild(sound);
+        return sound;
     }
 }
